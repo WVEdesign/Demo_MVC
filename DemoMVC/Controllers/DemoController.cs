5bb2bf1696f10e6919ac711d69de8741806c5cb1
Jesse <jesvis@bitconsult.be>
2021-07-14 14:15:18 +0200

    Add project files.

---
+++ DemoMVC/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            AutoModel autoModel = new AutoModel();
+            autoModel.KilometerStand = 150000;
+            autoModel.Merk = "Toyota";
+            autoModel.Type = "Auris";
+
+            AutoModel autoModel2 = new AutoModel();
+            autoModel2.KilometerStand = 20;
+            autoModel2.Merk = "BMW";
+            autoModel2.Type = "109D";
+
+            List<AutoModel> autolijst = new List<AutoModel>();
+            autolijst.Add(autoModel2);
+            autolijst.Add(autoModel);
+
+            return View(autolijst);
+        }
+    }
+}
---
+++ DemoMVC/Models/AutoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMVC.Models
+{
+    public class AutoModel
+    {
+        public int KilometerStand { get; set; }
+        public string Merk { get; set; }
+        public string Type { get; set; }
+    }
+}
